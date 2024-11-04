1392122abc2145dbf9688b409eb73b793bc469ca
Lado Asambadze <lado.asambadze1@gmail.com>
2024-11-04 14:04:31 +0400

    add metadata to contact

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,30 +1,28 @@
 import { getTranslations } from "next-intl/server";
 import ContactCard from "./_components/ContactCard";
+import { Locale } from "@/src/i18n/routing";
 
-export async function generateMetadata() {
-  // const { locale } = await params;
-  // const t = await getTranslations("meta");
-  // return {
-  //   title: t("contact"),
-  //   description: t("descriptionContact"),
-  //   openGraph: {
-  //     title: t("contact"),
-  //     description: t("descriptionContact"),
-  //     type: "website",
-  //     locale: locale,
-  //     url: "https://www.chateauiveri.ge/contact",
-  //     siteName: "Chateau Iveri",
-  //   },
-  // };
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
+  const t = await getTranslations("meta");
+  return {
+    title: t("contact"),
+    description: t("descriptionContact"),
+    openGraph: {
+      title: t("contact"),
+      description: t("descriptionContact"),
+      type: "website",
+      locale: locale,
+      url: "https://www.daudtravel.com/contact",
+      siteName: "Daud Travel",
+    },
+  };
 }
-
-// Mark the page component as async to use server components
 export default async function page() {
-  // You can also get translations directly in the page component if needed
-  const t = await getTranslations("contact");
-
-  console.log(t("hello"));
-
   return (
     <>
       <ContactCard />

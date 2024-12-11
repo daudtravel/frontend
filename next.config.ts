76f9ed03b3158a2f61c97ca5b89fc86a99f28b63
Lado <lado.asambadze1@gmail.com>
2024-12-11 19:55:23 +0400

    test build 2

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,24 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
-  // webpack(config: { module: { rules: { test: RegExp; use: { loader: string; options: { icon: boolean; }; }; }[]; }; }) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: { loader: "@svgr/webpack", options: { icon: true } },
-  //   });
-
-  //   return config;
+  // experimental: {
+  //   turbo: {
+  //     rules: {
+  //       "*.svg": {
+  //         loaders: ["@svgr/webpack"],
+  //         as: "*.js",
+  //       },
+  //     },
+  //   },
   // },
+  webpack(config: { module: { rules: { test: RegExp; use: { loader: string; options: { icon: boolean; }; }; }[]; }; }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: { loader: "@svgr/webpack", options: { icon: true } },
+    });
+
+    return config;
+  },
   images: {
     domains: ["localhost", "chateauiveri.ge"], // Add domains for image optimization
   },

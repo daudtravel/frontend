21d75411e681332eb87b9158e3d490ae630b0f27
Lado <lado.asambadze1@gmail.com>
2024-12-12 12:10:31 +0400

    revert last changes

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,29 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   turbo: {
-  //     rules: {
-  //       "*.svg": {
-  //         loaders: ["@svgr/webpack"],
-  //         as: "*.js",
-  //       },
-  //     },
-  //   },
-  // },
-  // webpack(config: { module: { rules: { test: RegExp; use: { loader: string; options: { icon: boolean; }; }; }[]; }; }) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: { loader: "@svgr/webpack", options: { icon: true } },
-  //   });
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack(config: { module: { rules: { test: RegExp; use: { loader: string; options: { icon: boolean; }; }; }[]; }; }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: { loader: "@svgr/webpack", options: { icon: true } },
+    });
 
-  //   return config;
-  // },
+    return config;
+  },
+  
   images: {
-    domains: ["localhost", "chateauiveri.ge"], // Add domains for image optimization
+    domains: ["localhost",  ],  
   },
 };
 
-// Apply the NextIntl plugin and export the combined configuration
+ 
 export default withNextIntl(nextConfig);

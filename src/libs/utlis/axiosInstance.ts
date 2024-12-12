45a4cb72d69c1aaf628bfc8b4805487c81130e88
Lado <lado.asambadze1@gmail.com>
2024-12-12 17:19:35 +0400

    updata url api

--- src/libs/utlis/axiosInstance.ts
+++ src/libs/utlis/axiosInstance.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 
+// export  const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:3000/api', 
+//     withCredentials: true, 
+//   });
+
 export  const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api', 
-    withCredentials: true, 
-  });
+  baseURL: 'https://daudtravel.com/api', 
+  withCredentials: true, 
+});

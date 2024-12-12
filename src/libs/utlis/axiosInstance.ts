cf24ac50567c3f2da813697b5ac829702e494399
Lado <lado.asambadze1@gmail.com>
2024-12-12 18:53:47 +0400

    modify axiosinstance

--- src/libs/utlis/axiosInstance.ts
+++ src/libs/utlis/axiosInstance.ts
@@ -6,7 +6,12 @@ import axios from "axios";
 //     withCredentials: true, 
 //   });
 
-export  const axiosInstance = axios.create({
+ 
+
+export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL, 
-  withCredentials: true, 
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });

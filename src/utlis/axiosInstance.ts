bd59e95074ef77c9a0974ccfd1424c8b3f69db87
Lado <lado.asambadze1@gmail.com>
2025-01-10 20:51:42 +0400

    add headers for cors

--- src/utlis/axiosInstance.ts
+++ src/utlis/axiosInstance.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  withCredentials: true,  // Add this if you're using cookies
   headers: {
     'Content-Type': 'application/json'
   }

a47494facc77901140e459f69c930641b49e3eb2
Lado <lado.asambadze1@gmail.com>
2025-10-21 00:40:32 +0400

    ????

--- src/app/[locale]/payment/_components/PaymentStatusCard.tsx
+++ src/app/[locale]/payment/_components/PaymentStatusCard.tsx
@@ -274,7 +274,7 @@ const PaymentStatusCard: React.FC<PaymentStatusCardProps> = ({
 
           <div className="bg-yellow-50 border border-yellow-200 rounded-md p-3">
             <p className="text-yellow-800 text-sm">
-              💡 Don t worry! No money has been charged to your account.
+              💡 Don t worry, No money has been charged to your account.
             </p>
           </div>
 

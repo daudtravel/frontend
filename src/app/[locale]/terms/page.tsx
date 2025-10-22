464ff674035ccf125fd6a1525c20353260faa7ad
Lado <lado.asambadze1@gmail.com>
2025-10-22 14:26:41 +0400

    remove font inter

--- src/app/[locale]/terms/page.tsx
+++ src/app/[locale]/terms/page.tsx
@@ -241,7 +241,7 @@ const TermsAndConditions = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-50 p-4 sm:p-8 font-['Inter']">
+    <div className="min-h-screen bg-gray-50 p-4 sm:p-8">
       <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-xl p-6 md:p-10">
         <h1 className="text-3xl md:text-4xl mb-8 pb-4 border-b-2">
           წესები და პირობები
